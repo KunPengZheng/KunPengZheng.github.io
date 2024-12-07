98b5e609be5ba81628e5c21f65c2d858db2abd32
gh <gh@gmail.com>
2024-12-07 15:17:57 +0800

    .

--- dist/xlsp/assets/js/ic_placeholder-8a7ae8d9.js
+++
@@ -1,1 +0,0 @@
-const e=""+new URL("../webp/name-a21339a4.webp",import.meta.url).href;export{e as i};
--- dist/xlsp/assets/js/use-id-c07ea722.js
+++
@@ -1,1 +0,0 @@
-import{bF as e}from"./index-fdc685fa.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
